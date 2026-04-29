92f2b16cf675fbee82d00ef9bf799e9bf96e091d
Leo1307 <leoperezsandoval@estudiantec.cr>
2026-04-29 16:12:30 -0600

    Prueba Tiny   Co-authored-by: Leonardo Pérez<leoperezsandoval@estudiantec.cr>

--- src/project.v
+++ src/project.v
@@ -1,11 +1,7 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
 
 `default_nettype none
 
-module tt_um_example (
+module mux_prueba (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,11 +13,21 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uo_out [7:4] = 0;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
-  assign uio_oe  = 0;
+  assign uio_oe [7:1] = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, clk, rst_n, uio_in[7:1], 1'b0};
+
+  assign uio_oe[0] = 1'b0;
+
+  mux_2to1_4b U0(
+    .a_i    (ui_in[3:0]),
+    .b_i    (ui_in[7:4]),
+    .s_i    (ui_in[0]),
+    .q_o    (ui_in[3:0])
+
+  );
 
 endmodule
